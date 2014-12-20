6f619e39b05e46d1fccb7e5add98c32a4ecd7dfe
zoot123 <t.woywod@web.de>
2014-12-20 11:44:37 +0100

    event destroy Image und event Club select

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,21 @@
 class Event < ActiveRecord::Base
   belongs_to :club
   mount_uploader :imageURL, ImageUploader
+
+  before_destroy :remember_image
+  after_destroy :remove_img
+
+  protected
+  def remember_image
+    @image_name = self[:imageURL]
+  end
+
+  def remove_img
+    if @image_name.present?
+      if File.exist?(@image_name)
+          File.delete(@image_name)
+      end
+    end
+  end
+
 end
