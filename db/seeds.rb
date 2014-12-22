36262d3cb09c67aa92c6bb1693989b147b31237a
zoot123 <t.woywod@web.de>
2014-12-23 00:55:33 +0100

    Optionen im application Controller link to Club und Events

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.new(email: "beer@beer.com", password: 'Hallo123', password_confirmation: 'Hallo123', first_name: 'Peter', last_name: 'Mueller')
-Club.create(name: "Test Club")
-Event.create(title: "Test", sdate: Date.current, club_id: Club.first.id, imageURL: File.new("public/uploads/event/image.jpg"))
+ad = Address.new(city:  'Münster' , postal_code: '48167', street: 'Straße', house_number: '11', country: 'Germany' )
+us = User.create(:email => "beer@beer.com", :password => 'Hallo123', :password_confirmation => 'Hallo123', :first_name => 'Peter', :last_name => 'Mueller', :address_id => ad.id)
+cl = Club.create(name: "Test Club" , address_id: ad.id , user_id: us.id)
+Event.create(title: "Test", sdate: Date.current, club_id: cl.id, imageURL: File.new("public/uploads/event/image.jpg"))
