487d9527f3cbcb9a45fc386bd2964140a532f775
Hirtenknogger <jan.wendt@online.de>
2015-01-08 13:47:14 -0800

    Turbolinks für ActiveAdmin deaktiviert

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -171,7 +171,7 @@ ActiveAdmin.setup do |config|
   #
   # To load a javascript file:
   #   config.register_javascript 'my_javascript.js'
-
+  config.register_javascript 'active_admin.js.coffee'
   # == CSV options
   #
   # Set the CSV builder separator
