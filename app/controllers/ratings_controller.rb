3bbe0a364006ceb1591e479c217e2872c0501138
marcelvoss95 <marcel.voss@web.de>
2015-01-12 08:52:13 -0800

    ratings gedryt und logik in controller

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,5 +1,28 @@
 class RatingsController < InheritedResources::Base
 
+
+  def index
+    @ratings1 = Rating.select("*,(Round(sum(value)/count(value),1)) as 'avgprice' ").joins(:beer).joins(:criterions).where(:criterions => {name: "Price"}).group("beers.id,criterions.name")
+    @ratings2 = Rating.select("*,(Round(sum(value)/count(value),1)) as 'avgtaste' ").joins(:beer).joins(:criterions).where(:criterions => {name: "Taste"}).group("beers.id,criterions.name")
+    @ratings3 = Rating.select("*,(Round(sum(value)/count(value),1)) as 'avgheadache' ").joins(:beer).joins(:criterions).where(:criterions => {name: "Headache"}).group("beers.id,criterions.name")
+    @ratings4 = Rating.select("*,(Round(sum(value)/count(value),1)) as 'overall' ").joins(:beer).joins(:criterions).group("beers.id")
+    @ratings5 = @ratings4
+
+    case params[:order]
+      when "Price" then @ratings1 = @ratings1.order('avgprice DESC')
+                        @ratings5 = @ratings1
+      when "Taste" then @ratings2 = @ratings2.order('avgtaste DESC')
+                        @ratings5 = @ratings2
+      when "Headache" then @ratings3 = @ratings3.order('avgheadache DESC')
+                           @ratings5 = @ratings3
+      when "Overall" then @ratings4 = @ratings4.order('overall DESC')
+                          @ratings5 = @ratings4
+    end
+
+    @ratings = Rating.joins(:beer).all.order("title")
+  end
+
+
   def new
     @rating = Rating.new
     @rating.criterions.build
