8520d3c05c0e56f4ef39f337fed2e1d964299bc3
marcelvoss95 <marcel.voss@web.de>
2015-02-08 09:28:25 -0800

    rating und user validations

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,46 +2,46 @@ class RatingsController < InheritedResources::Base
 
 
   def index
-  if Rails.env.development?
-    if (params[:type] == "avg")
-      @ratings1 = Rating.select("*,(Round(sum(value)/count(value),1)) as 'avgprice' ").joins(:beer).joins(:criterions).where(:criterions => {name: "Price"}).group("beers.id,criterions.name")
-      @ratings2 = Rating.select("*,(Round(sum(value)/count(value),1)) as 'avgtaste' ").joins(:beer).joins(:criterions).where(:criterions => {name: "Taste"}).group("beers.id,criterions.name")
-      @ratings3 = Rating.select("*,(Round(sum(value)/count(value),1)) as 'avgheadache' ").joins(:beer).joins(:criterions).where(:criterions => {name: "Headache"}).group("beers.id,criterions.name")
-      @ratings4 = Rating.select("*,(Round(sum(value)/count(value),1)) as 'overall' ").joins(:beer).joins(:criterions).group("beers.id")
-      @ratings5 = @ratings4
+    if Rails.env.development?
+      if (params[:type] == "avg")
+        @ratings1 = Rating.select("*,(Round(sum(value)/count(value),1)) as 'avgprice' ").joins(:beer).joins(:criterions).where(:criterions => {name: "Price"}).group("beers.id,criterions.name")
+        @ratings2 = Rating.select("*,(Round(sum(value)/count(value),1)) as 'avgtaste' ").joins(:beer).joins(:criterions).where(:criterions => {name: "Taste"}).group("beers.id,criterions.name")
+        @ratings3 = Rating.select("*,(Round(sum(value)/count(value),1)) as 'avgheadache' ").joins(:beer).joins(:criterions).where(:criterions => {name: "Headache"}).group("beers.id,criterions.name")
+        @ratings4 = Rating.select("*,(Round(sum(value)/count(value),1)) as 'overall' ").joins(:beer).joins(:criterions).group("beers.id")
+        @ratings5 = @ratings4
+
+
+      else
+        @ratings1 = Rating.select("*,value as 'avgprice' ").joins(:beer).joins(:criterions).where(:criterions => {name: "Price"}).group("ratings.id")
+        @ratings2 = Rating.select("*,value as 'avgtaste' ").joins(:beer).joins(:criterions).where(:criterions => {name: "Taste"}).group("ratings.id")
+        @ratings3 = Rating.select("*,value as 'avgheadache' ").joins(:beer).joins(:criterions).where(:criterions => {name: "Headache"}).group("ratings.id")
+        @ratings4 = Rating.select("*,(Round(sum(value)/3,1)) as 'overall' ").joins(:criterions).joins(:beer).group("ratings.id")
+        @ratings5 = @ratings4
+
+      end
 
 
     else
-      @ratings1 = Rating.select("*,value as 'avgprice' ").joins(:beer).joins(:criterions).where(:criterions => {name: "Price"}).group("ratings.id")
-      @ratings2 = Rating.select("*,value as 'avgtaste' ").joins(:beer).joins(:criterions).where(:criterions => {name: "Taste"}).group("ratings.id")
-      @ratings3 = Rating.select("*,value as 'avgheadache' ").joins(:beer).joins(:criterions).where(:criterions => {name: "Headache"}).group("ratings.id")
-      @ratings4 = Rating.select("*,(Round(sum(value)/3,1)) as 'overall' ").joins(:criterions).joins(:beer).group("ratings.id")
-      @ratings5 = @ratings4
+
+
+      if (params[:type] == "avg")
+        @ratings1 = Rating.select("beers.id,beers.id as beer_id,(Round(sum(value)::numeric/count(value)::numeric,1)) as avgprice ").joins(:beer).joins(:criterions).where(:criterions => {name: "Price"}).group("beers.id,criterions.name")
+        @ratings2 = Rating.select("beers.id,beers.id as beer_id,(Round(sum(value)::numeric/count(value)::numeric,1)) avgtaste ").joins(:beer).joins(:criterions).where(:criterions => {name: "Taste"}).group("beers.id,criterions.name")
+        @ratings3 = Rating.select("beers.id,beers.id as beer_id,(Round(sum(value)::numeric/count(value)::numeric,1)) avgheadache ").joins(:beer).joins(:criterions).where(:criterions => {name: "Headache"}).group("beers.id,criterions.name")
+        @ratings4 = Rating.select("beers.id,beers.id as beer_id,(Round(sum(value)::numeric/count(value)::numeric,1)) as overall ").joins(:beer).joins(:criterions).group("beers.id")
+        @ratings5 = @ratings4
+
+
+      else
+        @ratings1 = Rating.select("ratings.id,headline,comment,ratings.id as rating_id,beer_id,user_id,value as avgprice ").joins(:beer).joins(:criterions).where(:criterions => {name: "Price"}).group("ratings.id,criterions.value")
+        @ratings2 = Rating.select("ratings.id,headline,comment,ratings.id as rating_id,beer_id,user_id,value as avgtaste ").joins(:beer).joins(:criterions).where(:criterions => {name: "Taste"}).group("ratings.id,criterions.value")
+        @ratings3 = Rating.select("ratings.id,headline,comment,ratings.id as rating_id,beer_id,user_id,value as avgheadache ").joins(:beer).joins(:criterions).where(:criterions => {name: "Headache"}).group("ratings.id,criterions.value")
+        @ratings4 = Rating.select("ratings.id,headline,comment,ratings.id as rating_id,beer_id,user_id,(Round(sum(value)::numeric/3,1)) AS overall ").joins(:criterions).joins(:beer).group("ratings.id")
+        @ratings5 = @ratings4
+
+      end
 
     end
-
-
-  else
-
-
-    if (params[:type] == "avg")
-    @ratings1 = Rating.select("beers.id,beers.id as beer_id,(Round(sum(value)::numeric/count(value)::numeric,1)) as avgprice ").joins(:beer).joins(:criterions).where(:criterions => {name: "Price"}).group("beers.id,criterions.name")
-    @ratings2 = Rating.select("beers.id,beers.id as beer_id,(Round(sum(value)::numeric/count(value)::numeric,1)) avgtaste ").joins(:beer).joins(:criterions).where(:criterions => {name: "Taste"}).group("beers.id,criterions.name")
-    @ratings3 = Rating.select("beers.id,beers.id as beer_id,(Round(sum(value)::numeric/count(value)::numeric,1)) avgheadache ").joins(:beer).joins(:criterions).where(:criterions => {name: "Headache"}).group("beers.id,criterions.name")
-    @ratings4 = Rating.select("beers.id,beers.id as beer_id,(Round(sum(value)::numeric/count(value)::numeric,1)) as overall ").joins(:beer).joins(:criterions).group("beers.id")
-    @ratings5 = @ratings4
-
-
-    else
-      @ratings1 = Rating.select("ratings.id,headline,comment,ratings.id as rating_id,beer_id,user_id,value as avgprice ").joins(:beer).joins(:criterions).where(:criterions => {name: "Price"}).group("ratings.id,criterions.value")
-      @ratings2 = Rating.select("ratings.id,headline,comment,ratings.id as rating_id,beer_id,user_id,value as avgtaste ").joins(:beer).joins(:criterions).where(:criterions => {name: "Taste"}).group("ratings.id,criterions.value")
-      @ratings3 = Rating.select("ratings.id,headline,comment,ratings.id as rating_id,beer_id,user_id,value as avgheadache ").joins(:beer).joins(:criterions).where(:criterions => {name: "Headache"}).group("ratings.id,criterions.value")
-      @ratings4 = Rating.select("ratings.id,headline,comment,ratings.id as rating_id,beer_id,user_id,(Round(sum(value)::numeric/3,1)) AS overall ").joins(:criterions).joins(:beer).group("ratings.id")
-      @ratings5 = @ratings4
-
-    end
-
-  end
 
     case params[:order]
       when "Price" then @ratings1 = @ratings1.order('avgprice DESC')
@@ -63,11 +63,47 @@ class RatingsController < InheritedResources::Base
     @rating.criterions.build
   end
 
+  def create
+    @rating = Rating.new(rating_params)
+
+    respond_to { |format|
+      if @rating.save
+
+        format.html { redirect_to @rating, notice: "Rating was successfully created." }
+        format.json { render :show, status: :created, location: @rating }
+      else
+
+        @beerid = rating_params[:beer_id]
+
+        format.html { render :new }
+        format.json { render json: @rating.errors, status: :unprocessable_entity }
+      end
+    }
+  end
+
+  def edit
+    @rating = Rating.find(params[:id])
+  end
+
+
+  def update
+    @rating = Rating.find(params[:id])
+    respond_to do |format|
+      if @rating.update(rating_params)
+        format.html { redirect_to ratings_path, notice: 'Rating was successfully updated.' }
+        format.json { render :index, status: :ok, location: @rating }
+      else
+        @beerid = rating_params[:beer_id]
+        format.html { render :edit }
+        format.json { render json: @rating.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
 
   private
 
-    def rating_params
-      params.require(:rating).permit(:beer_id, :user_id,:headline, :comment, criterions_attributes:[:id,:rating_id, :name, :value])
-    end
+  def rating_params
+    params.require(:rating).permit(:beer_id, :user_id,:headline, :comment, criterions_attributes:[:id,:rating_id, :name, :value])
+  end
 end
--- app/models/criterion.rb
+++ app/models/criterion.rb
@@ -1,3 +1,4 @@
 class Criterion < ActiveRecord::Base
   belongs_to :rating
+  validates :value ,:inclusion => 1..5
 end
