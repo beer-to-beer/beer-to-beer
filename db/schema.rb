05f60d77b25bce69bf292ee69c54bef202045bd3
Hirtenknogger <jan.wendt@online.de>
2015-02-10 09:39:29 -0800

    seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150209124417) do
+ActiveRecord::Schema.define(version: 20150123093600) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
