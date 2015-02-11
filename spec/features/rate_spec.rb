8d9d988ccf8a76c062989d43580d7a2129e6d41b
marcelvoss95 <marcel.voss@web.de>
2015-02-11 13:59:20 -0800

    rate specs

--- spec/features/rate_spec.rb
+++ spec/features/rate_spec.rb
@@ -1,27 +1,15 @@
 require 'rails_helper'
+
 describe 'Ratings' do
 
-  let!(:rating1) { FactoryGirl.create(:rating) }
-  let!(:criterion1) { FactoryGirl.create(:criterion, name: "Price", rating_id: Rating.first.id) }
-  let!(:criterion2) { FactoryGirl.create(:criterion, name: "Taste", rating_id: Rating.first.id) }
-  let!(:criterion3) { FactoryGirl.create(:criterion, name: "Headache", rating_id: Rating.first.id) }
-  let!(:user) { FactoryGirl.create(:user) }
-  let!(:beer) { FactoryGirl.create(:beer) }
 
-  it 'could be created' do
-
-    expect { FactoryGirl.create(:rating) }.to change(Rating, :count).by(1)
-  end
-
-  it 'could be deleted' do
-    expect { Capybara.current_session.driver.delete rating_path(rating1.id) }.to change(Rating, :count).by(-1)
-  end
 
   it 'can be created via form' do
-
+    user = FactoryGirl.create(:user)
+    beer = FactoryGirl.create(:beer)
     visit root_path
     click_link 'Sign in'
-    fill_form(:user, {email: 'test@test.de', password: 'Hallo123'})
+    fill_form(:user, {email: user.email, password: 'Hallo123'})
     click_button 'Sign in'
 
     visit new_rating_path(:beer_id => beer.id)
@@ -31,7 +19,7 @@ describe 'Ratings' do
     select 2.0, :from => "headache"
 
     expect { click_button 'Create Rating' }.to change { Rating.count }.by(1)
-    expect(page).to have_content 'test@test.de'
+    expect(page).to have_content user.email
     expect(page).to have_content beer.title
     expect(page).to have_content 'myHeadline'
     expect(page).to have_content 'myComment'
@@ -42,15 +30,20 @@ describe 'Ratings' do
 
   end
   it 'can be edited via form' do
-
+    user = FactoryGirl.create(:user)
+    beer = FactoryGirl.create(:beer)
     visit root_path
     click_link 'Sign in'
-    fill_form(:user, {email: 'test@test.de', password: 'Hallo123'})
+    fill_form(:user, {email: user.email, password: 'Hallo123'})
     click_button 'Sign in'
 
-    visit rating_path(:id => 1)
-
+    rating2 = FactoryGirl.create(:rating,headline: 'myHeadline',comment: 'myCommment',user_id: user.id)
+    FactoryGirl.create(:criterion, name: "Price", rating_id: rating2.id, value:4.0)
+    FactoryGirl.create(:criterion, name: "Taste", rating_id: rating2.id,value:4.0)
+    FactoryGirl.create(:criterion, name: "Headache", rating_id: rating2.id,value:4.0)
+    visit rating_path(rating2)
     click_link 'Edit'
+
     fill_form(:rating, {headline: 'myHeadline2', comment: 'myComment2'})
 
     select 1.0, :from => "price"
@@ -61,22 +54,36 @@ describe 'Ratings' do
 
     expect(page).to have_content 'myHeadline2'
     expect(page).to have_content 'myComment2'
+
     expect(find(:css, 'select#criterion_Price').value).to eq('1.0')
     expect(find(:css, 'select#criterion_Taste').value).to eq('2.0')
     expect(find(:css, 'select#criterion_Headache').value).to eq('3.0')
   end
-  it 'can be edited via form' do
 
+
+  it 'can be deleted via form',:js => true do
+    user = FactoryGirl.create(:user)
+    beer = FactoryGirl.create(:beer)
     visit root_path
     click_link 'Sign in'
-    fill_form(:user, {email: 'test@test.de', password: 'Hallo123'})
+    fill_form(:user, {email: user.email, password: 'Hallo123'})
     click_button 'Sign in'
 
-    visit ratings_path()
+    rating2 = FactoryGirl.create(:rating,headline: 'myHeadline',comment: 'myCommment',user_id: user.id)
+    FactoryGirl.create(:criterion, name: "Price", rating_id: rating2.id, value:4.0)
+    FactoryGirl.create(:criterion, name: "Taste", rating_id: rating2.id,value:4.0)
+    FactoryGirl.create(:criterion, name: "Headache", rating_id: rating2.id,value:4.0)
+
+    visit rating_path(rating2)
 
 
-    expect { click_link 'Destroy' }.to change(Rating, :count).by(-1)
-
+    expect {
+      click_link('Destroy')
+      sleep 10.seconds
+      alert = page.driver.browser.switch_to.alert
+      alert.accept
+      sleep 10.seconds
+    }.to change(Rating, :count).by(-1)
 
   end
 end
