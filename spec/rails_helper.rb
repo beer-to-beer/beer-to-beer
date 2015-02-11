4315b9792f0f22faa53bcb0043f4ffcc2f052e5d
marcelvoss95 <marcel.voss@web.de>
2015-02-11 14:03:17 -0800

    helper angepasst

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
-
 require 'support/integration_test_helper'
 require 'devise'
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -35,7 +34,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  #config.use_transactional_fixtures = true
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -52,5 +51,11 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
   config.include Formulaic::Dsl, type: :feature
+
+
+
+
+
+
 
 end
