d3071d76d7e06f148f5b1aee444d1eb8982e9f2b
marcelvoss95 <marcel.voss@web.de>
2015-02-11 13:59:10 -0800

    club specs

--- spec/features/clubs_spec.rb
+++ spec/features/clubs_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 describe "Clubs", :type => :feature do
-  let!(:user) {
-    FactoryGirl.create(:user, address: FactoryGirl.create(:address))
-    user.add_role :admin
-  }
-  let!(:address) { FactoryGirl.create(:address)}
-  let!(:club1) { FactoryGirl.create(:club, address: FactoryGirl.create(:address), user: user)}
+
+
   #let!(:events) { FactoryGirl.create(:event, club_id: Club.first.id)}
+  #address= FactoryGirl.create(:address)
+  #club1=FactoryGirl.create(:club, address: FactoryGirl.create(:address), user: user)
 
 
   before(:each) do
-    visit root_path
-    click_link 'Sign in'
-    fill_in 'user_email', with: 'admin'
-    fill_in 'user_password', with: 'Hallo123'
-    click_button 'Sign in'
-    expect(page).to have_content 'Signed in successfully'
+
+
+   #kann hier wieder rein, bin jetzt zu faul
+
+
+
+    #expect(page).to have_content user.email
   end
 
   after(:each) do
@@ -23,30 +22,50 @@ describe "Clubs", :type => :feature do
   end
 
   it 'could be added' do
+    user = FactoryGirl.create(:user)
+    user.add_role :admin
+    visit root_path
+    click_link 'Sign in'
+    fill_form(:user, {email: user.email, password: 'Hallo123'})
+    click_button 'Sign in'
+
     visit new_club_path
 
     fill_form(:club, {name: Faker::Name.name, city: Faker::Address.city, postal_code: Faker::Address.postcode,
                       street: Faker::Address.street_name, house_number: Faker::Address.building_number,
                       country: Faker::Address.country, url: Faker::Internet.url, active: true})
 
-    save_and_open_page
-    expect{ click_on('Create Club') }.to change(Club, :count).by(1)
+    expect{ click_button('Create Club') }.to change(Club, :count).by(1)
   end
 
 
-  it 'could be deleted club' do
+  it 'could be deleted club',:js => true do
+    user = FactoryGirl.create(:user)
+    user.add_role :admin
+    visit root_path
+    click_link 'Sign in'
+    fill_form(:user, {email: user.email, password: 'Hallo123'})
+    click_button 'Sign in'
 
-    club2 =  FactoryGirl.create(:club, address_id: Address.first.id)
+
+    club2 =  FactoryGirl.create(:club, active:true,user: user)
 
     visit clubs_path
-    #save_and_open_page
+
     #expect(page.has_content?('Destroy'))
 
-    click_link('Destroy', href: club_path(club2))
-    expect { find_button('OK').click }.to change(Club, :count).by(-1)
 
-    #expect{Capybara.current_session.driver.delete club_path(club1.id)}.to change(Club, :count).by(-1)
+    expect {
+      click_link('Destroy', href: club_path(club2))
+      sleep 10.seconds
+      alert = page.driver.browser.switch_to.alert
+      alert.accept
+      sleep 10.seconds
+    }.to change(Club, :count).by(-1)
+
   end
+
+
   it 'could be deleted and delete Address' do
     expect{Capybara.current_session.driver.delete club_path(club1.id)}.to change(Address, :count).by(-1)
   end
