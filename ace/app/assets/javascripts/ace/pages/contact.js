a4ff6d00cfe5a1a5c8dd0099290c6438afbd235b
Hirtenknogger <jan.wendt@online.de>
2015-02-13 03:01:54 -0800

    GMap geolocate implementiert

--- ace/app/assets/javascripts/ace/pages/contact.js
+++ ace/app/assets/javascripts/ace/pages/contact.js
@@ -1,23 +1,33 @@
 var Contact = function () {
 
     return {
-        
-        //Map
-        initMap: function () {
-			var map;
-			$(document).ready(function(){
-			  map = new GMaps({
-				div: '#map',
-				lat: -13.004333,
-				lng: -38.494333
-			  });
-			   var marker = map.addMarker({
-		            lat: -13.004333,
-					lng: -38.494333,
-		            title: 'Loop, Inc.'
-		        });
-			});
-        }
 
+        //Map
+        initMap: function (argumentaddress) {
+            var map;
+            $(document).ready(function(){
+
+                map = new GMaps({
+                    div: '#map',
+                    lat: -12.043333,
+                    lng: -77.028333
+                });
+
+                GMaps.geocode({
+                    address: String(argumentaddress),
+                    callback: function(results, status){
+                        if(status=='OK'){
+                            var latlng = results[0].geometry.location;
+                            map.setCenter(latlng.lat(), latlng.lng());
+                            map.addMarker({
+                                lat: latlng.lat(),
+                                lng: latlng.lng()
+                            });
+                        }
+                    }
+                });
+
+            });
+        }
     };
 }();
