50d401a29fadc4e3c7e834eeb9018942a534aaf6
marcelvoss95 <marcel.voss@web.de>
2015-02-14 11:59:54 -0800

    default role

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,12 @@ class User < ActiveRecord::Base
   validates_associated :address
 
 
+  after_create :assign_default_role
+
+  def assign_default_role
+    add_role(:user)
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
