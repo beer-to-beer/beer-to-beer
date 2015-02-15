fc507e20ad6f70650b8a847df526782844a80261
zoot123 <t.woywod@web.de>
2015-02-15 10:47:04 +0100

    rspec event

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -1,7 +1,5 @@
 require 'rails_helper'
 require 'support/integration_test_helper'
-
-
 describe 'Event' , :type => :feature do
   let!(:club1) {FactoryGirl.create(:club,active: true)}
   it 'could be added' do
@@ -10,16 +8,17 @@ describe 'Event' , :type => :feature do
     club1.user= user
     date = Faker::Time.forward(23, :morning)
     save_and_open_page
-    fill_form(:event, {
-                        title: Faker::Name.title,
-                        event_sdate_1i: select(date.year),
-                        event_sdate_2i: select(date.month),
-                        event_sdate_3i: select(date.day),
-                        event_sdate_4i: select(date.hour),
-                        event_sdate_5i: select(date.min),
+    fill_form(:event,:new, {
+                        title: input('event_title', Faker::Name.title.to_s),
+                        sdate_1i: select(date.strftime('%Y'), from: 'event_sdate_1i'),
+                        sdate_2i: select(date.strftime('%B'), from: 'event_sdate_2i'),
+                        sdate_3i: select(date.strftime('%e').delete(' '), from: 'event_sdate_3i'),
+                        sdate_4i: select(date.strftime('%H'), from: 'event_sdate_4i'),
+                        sdate_5i: select(date.strftime('%M'), from: 'event_sdate_5i'),
                         imageURL: File.new("#{Rails.root}/ace/app/assets/img/ace/uploads/event/image1.jpg"),
-                        club: select(club1.name, from: 'event_club_id')
-                    })
+                        club_id: select(club1.name)
+                    }
+    )
     save_and_open_page
     #Save_and_Open_Page
     expect{click_link 'Create Event'}.to change(Event, :count).by(1)
