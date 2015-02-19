57b655a4c522d2276a4ebe1c6807128429fac15a
marcelvoss95 <marcel.voss@web.de>
2015-02-19 10:28:04 -0800

    address validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,5 @@ class Address < ActiveRecord::Base
   has_one :user
   has_one :club
   has_one :shop
-  #validates :street,:house_number,:city,:country, presence: true
-  #validates :postal_code,presence: true,numericality: true
+  validates :street,:house_number,:city,:country,:postal_code, presence: true
 end
