da32545d698538ccf6ae9af346d557eeffbab882
marcelvoss95 <marcel.voss@web.de>
2015-02-24 10:55:34 -0800

    shop validations

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,5 +4,7 @@ class Shop < ActiveRecord::Base
   has_many :beers, through: :shop_beers
   belongs_to :user
   belongs_to :address
+  validates :name,:email,:phonenumber,:website, presence: true
+  validates_associated :address
   accepts_nested_attributes_for :address, :user
 end
