3197b499b9c3e0a2a6c81a1a9181ce274177a693
marcelvoss95 <marcel.voss@web.de>
2015-02-24 10:17:09 -0800

    shop test refactored

--- spec/factories/shops.rb
+++ spec/factories/shops.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :shop do
     name "MyString"
+    email Faker::Internet.email
+    phonenumber Faker::Number.number(10)
+    website Faker::Internet.domain_name
     address {FactoryGirl.create(:address)}
   end
 
--- spec/features/shop_spec.rb
+++ spec/features/shop_spec.rb
@@ -7,12 +7,17 @@ describe 'Shops' do
     beer2 = FactoryGirl.create(:beer)
     address = FactoryGirl.create(:address)
 
+
+    shop_mail = Faker::Internet.email
+    shop_phone = Faker::Number.number(10)
+    shop_website = Faker::Internet.domain_name
     visit new_shop_path
-    fill_form(:shop, {name: 'myShop'})
+    fill_form(:shop, {name: 'myShop', email: shop_mail , 'shop_phonenumber' => shop_phone,website: shop_website })
     fill_form(:address, { city: address.city, 'shop_address_attributes_postal_code'=> address.postal_code, street: address.street, house_number: address.house_number,country: address.country })
 
 
     expect { click_button 'Create Shop' }.to change { Shop.count }.by(1)
+
     expect(page).to have_content 'myShop'
     expect(page).to have_content address.city
     expect(page).to have_content address.postal_code
@@ -20,7 +25,9 @@ describe 'Shops' do
     expect(page).to have_content address.house_number
     expect(page).to have_content address.country
 
-
+    expect(page).to have_content shop_website
+    expect(page).to have_content shop_mail
+    expect(page).to have_content shop_phone
   end
 
   it 'can be edited' do
@@ -30,17 +37,20 @@ describe 'Shops' do
     address = FactoryGirl.create(:address)
     shop = FactoryGirl.create(:shop,user: user)
 
+    shop_mail = Faker::Internet.email
+    shop_phone = Faker::Number.number(10)
+    shop_website = Faker::Internet.domain_name
 
     visit shop_path(shop)
 
     click_link_or_button 'Edit'
-    fill_form(:shop, {name: 'myShop2'})
+    fill_form(:shop, {name: 'myShop2' , email: shop_mail , 'shop_phonenumber' => shop_phone, website: shop_website })
     fill_form(:address, { city: address.city, 'shop_address_attributes_postal_code'=> address.postal_code, street: address.street, house_number: address.house_number,country: address.country })
 
 
 
     click_link_or_button 'Update Shop'
-
+    visit shop_path(shop)
 
     expect(page).to have_content 'myShop2'
     expect(page).to have_content address.city
@@ -48,6 +58,9 @@ describe 'Shops' do
     expect(page).to have_content address.street
     expect(page).to have_content address.house_number
     expect(page).to have_content address.country
+    expect(page).to have_content shop_website
+    expect(page).to have_content shop_mail
+    expect(page).to have_content shop_phone
   end
 
   it 'can be deleted',:js => true do
